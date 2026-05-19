e56a79f63c7c408dd595b5d96b3dec634777043e
jballesteros28 <juandavidballesterosbayona@gmail.com>
2026-05-19 15:04:23 -0300

    arreglo final

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py crear_superuser_render
-python manage.py crear_usuarios_demo --render-safe
+python manage.py crear_usuarios_demo
